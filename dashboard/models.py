95d8d8d517a879dc6934912dc68e71af59e5cfe7
Dominic Domugho <dominicdumogho66@gmail.com>
2022-01-01 18:26:44 +0800

    [CRUD-005] - Made migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    CATEGORY =  (
+        ('Pillow', 'Pillow'),
+        ('Food', 'Food'),
+        ('Toys', 'Toys'),
+    )
+
+    name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    product_code = models.CharField(max_length=10, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField(blank=True, null=True)
+    category = models.CharField(max_length=10, null=True, choices=CATEGORY)
+
+    def __str__(self):
+        return self.name
